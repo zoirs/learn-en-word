ba964aa219e77fd955e73af2ec726a4edafa0a66
Denis <zoirs@ya.ru>
2025-10-21 23:06:00 +0300

    test

--- src/main/java/com/zoirs/learn_en_word/controller/InitController.java
+++ src/main/java/com/zoirs/learn_en_word/controller/InitController.java
@@ -1,15 +1,13 @@
 package com.zoirs.learn_en_word.controller;
 
 import com.zoirs.learn_en_word.req.InitSessionReq;
+import com.zoirs.learn_en_word.service.NotificationService;
 import com.zoirs.learn_en_word.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/session")
@@ -18,12 +16,21 @@ public class InitController {
     private static final Logger log = LoggerFactory.getLogger(InitController.class);
 
     @Autowired
+    private NotificationService notificationService;
+    @Autowired
     private UserService userService;
 
     @PostMapping("/init")
     public ResponseEntity<Boolean> createToken(@RequestBody InitSessionReq req) {
         log.info("Init user {}", req);
         userService.initUser(req.userId(), req.fireBaseToken(), req.timezone());
+        return ResponseEntity.ok(true);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Boolean> test() {
+        log.info("test notify");
+        notificationService.sendHourlyQuizzes();
         return ResponseEntity.ok(true);
     }
 }
