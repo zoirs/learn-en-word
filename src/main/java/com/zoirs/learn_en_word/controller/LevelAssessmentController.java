5b88081cfb75780afbc801637c885f1be7c79638
Denis <zoirs@ya.ru>
2025-10-25 12:56:23 +0300

    add setDifficultyLevel

--- src/main/java/com/zoirs/learn_en_word/controller/LevelAssessmentController.java
+++ src/main/java/com/zoirs/learn_en_word/controller/LevelAssessmentController.java
@@ -33,6 +33,21 @@ public class LevelAssessmentController {
     public ResponseEntity<List<Meaning>> getWordSuggestions() {
         List<Integer> ids = List.of(181891, 62337, 202759, 144646, 24452, 158980, 122630, 134538, 27657, 159881, 96141, 30610, 162704, 1938, 104853, 218132, 66333, 151198, 227998, 191137, 184993, 77223, 196389, 233641, 134057, 87724, 188460, 185522, 31920, 112178, 13109, 236469, 183618, 5825, 61001, 207434, 138447, 81871, 19918, 194381, 45009, 55378, 16726, 103001, 226138, 40794, 192984, 146399, 151773, 87011, 36834, 190309, 187115, 32873, 145645, 156016, 175732, 36603, 113022, 71806);
         List<Meaning> meanings = dictionaryCacheService.getMeanings(ids);
+        for (Meaning meaning : meanings) {
+            if (a1.contains(meaning.getText())) {
+                meaning.setDifficultyLevel(0);
+            } else if (a2.contains(meaning.getText())) {
+                meaning.setDifficultyLevel(1);
+            } else if (b1.contains(meaning.getText())) {
+                meaning.setDifficultyLevel(2);
+            } else if (b2.contains(meaning.getText())) {
+                meaning.setDifficultyLevel(3);
+            } else if (c1.contains(meaning.getText())) {
+                meaning.setDifficultyLevel(4);
+            } else if (c2.contains(meaning.getText())) {
+                meaning.setDifficultyLevel(5);
+            }
+        }
         return ResponseEntity.ok(meanings);
     }
 }
